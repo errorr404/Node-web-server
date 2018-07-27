596e0f159a78a63f9e0f5e8220f5b700e321fc55
errorr404 <dkbishwash@gmail.com>
2018-07-27 18:43:15 +0530

    creating web server and serve a dynamic html file

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 var app = express(); // here we initialize app vairable by calling express function
 
+app.use(express.static(__dirname + '/playground')); // here we servet the help.html page by middleware.
+
 // here get method takes two argument first the page path and second an arrow function.
 app.get('/',(req,res) => {
   res.send({
@@ -11,4 +13,16 @@ app.get('/',(req,res) => {
   }); // here data send to the page by send().
 });
 
-app.listen(3000); // here we bind the app with port of the machine.
+app.get('/about',(req,res) =>{
+  res.send('About page');
+});
+
+app.get('/bad',(req,res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(3000, ()=>{
+  console.log('App is running on the port 3000');
+}); // here we bind the app with port of the machine.
