40435b55454e313e7ceb886a717875765cdc3bf0
errorr404 <dkbishwash@gmail.com>
2018-07-27 19:27:38 +0530

    Adding the Handlebar pakage and rendering the templates with data

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 const express = require('express');
-
+const hbs = require('hbs'); // adding the bandlebars -> https://handlebarsjs.com/
 var app = express(); // here we initialize app vairable by calling express function
 
+app.set('view engine','hbs');   // Using hbs as the default view engine requires just one line of code in your app setup. This will render .hbs files when res.render is called
 app.use(express.static(__dirname + '/playground')); // here we servet the help.html page by middleware.
 
 // here get method takes two argument first the page path and second an arrow function.
 app.get('/',(req,res) => {
-  res.send({
+/*
+ res.send({
     name: 'dixit',
     middle: 'kumar',
     last: 'Bishwas'
   }); // here data send to the page by send().
+  */
+  res.render('home.hbs',{
+    pageName: 'Home page',
+    currentYear: new Date().getFullYear(),
+    welcomeMsg: 'welcome to our website !'
+  });
 });
 
 app.get('/about',(req,res) =>{
-  res.send('About page');
+  res.render('about.hbs', {
+    pageName: 'About Page',
+    currentYear: new Date().getFullYear()
+  }); // it will render the about.hbs page from view folder, here the second agument  in the render function passes the data in the about.hbs page
 });
 
 app.get('/bad',(req,res) => {
