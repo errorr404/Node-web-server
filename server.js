1cb976bed16d68b4104d166e38aaa584c0c73161
errorr404 <dkbishwash@gmail.com>
2018-07-28 12:52:56 +0530

    Use the express middleware

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const fs = require('fs');
 const hbs = require('hbs'); // adding the bandlebars -> https://handlebarsjs.com/
 var app = express(); // here we initialize app vairable by calling express function
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine','hbs');   // Using hbs as the default view engine requires just one line of code in your app setup. This will render .hbs files when res.render is called
+
+
+//app.use() --> this is how we declear the middleware in express-nodejs
+app.use((req,res,next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log',log+ '\n',(err) => {
+    if(err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next(); // it defines the next destination
+});
+
+
+/*
+// here i m going to write the middleware for Maintanance
+app.use((req,res,next) => {
+  res.render('maintanance.hbs'); // here after this line if we couldn't call next() then it will not execute the next lines...
+});
+*/
+
 app.use(express.static(__dirname + '/playground')); // here we servet the help.html page by middleware.
 
 // here this registerHelpers function takes two argument first the name of the helper and second the fuction which returns the value of the helper.
